1a648a57573a88613ed2403275e2edd2900df6de
wardner <wanerlara1999@gmail.com>
2021-05-03 12:43:07 -0400

    Cleanup the function

--- Docker Image/types.ts
+++
@@ -1,7 +0,0 @@
-export interface Request{
-    body:string|object;
-    headers:object;
-    method: string;
-    query: object;
-    path:string;
-}
--- Docker Image/utils/handlerManager.ts
+++
@@ -1,9 +0,0 @@
-import FunctionEvent from './functionEvent';
-import FunctionContext from './functionContext';
-import handler from '../function/handler';
-
-export default class HandlerManager {
-    static async handler(functionEvent: FunctionEvent, functionContext:FunctionContext){
-        return handler(functionEvent, functionContext);
-    }
-}
